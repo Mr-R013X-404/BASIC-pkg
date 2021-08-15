274de3d81bd6ad2cc075e222c2e9896401578239
S@BBIR <87950047+WHI73-D3VI1@users.noreply.github.com>
2021-08-16 01:54:46 +0600

    Add files via upload

---
+++ basic-pkg.sh
@@ -0,0 +1,149 @@
+echo 
+echo
+figlet -f slant 'W H I 7 3' |lolcat
+figlet -f slant 'D 3 V I 1' |lolcat
+echo
+echo
+
+echo "Wait For All Package Download."
+
+echo
+echo
+echo
+
+
+apt update
+
+apt upgrade
+
+termux-setup-storage
+
+pkg install python -y
+
+pkg install python2 -y
+
+pkg install python3 -y
+
+pkg install python-static -y
+
+pkg install python-tkinter -y
+
+pkg install vim-python -y
+
+pkg install python2-static -y
+
+pkg install weechat-python-plugin -y
+
+pkg install git -y
+
+pkg install figlet -y
+
+pkg install cmatrix -y
+
+pkg install nano -y
+
+pkg install php -y
+
+pkg install fish -y
+
+pkg install help -y
+
+pkg install host -y
+
+pkg install php -y
+
+pkg install perl -y
+
+pkg install nmap -y
+
+pkg install clang -y
+
+pip install astroid 
+
+pip install autopep8
+
+pip install certifi
+
+pip install chardet
+
+pip install colorama 
+
+pip install future
+
+pip install idna 
+
+pip install isort 
+
+pip install lazy-object-proxy
+
+pip install lolcat 
+
+pip install mccabe  
+
+pip install Pillow 
+
+pip install pilo 
+
+pip install pip
+
+pip install pycodestyle 
+
+pip install pyfiglet
+
+pip install pylint
+
+pip install requests 
+
+pip install setuptools
+
+pip install six
+
+pip install termcolor
+
+pip install toml
+
+pip install urllib3
+
+pip install wheel
+
+pip install wrapt
+
+pip install youtube-dl
+
+pip install mechanize 
+
+apt install ruby -y
+
+apt install openssh -y
+
+apt install wget -y
+
+apt install curl -y
+
+apt install proot -y
+
+pkg install java -y
+
+pkg install fish -y
+
+
+figlet -f slant 'W H I 7 3' |lolcat
+figlet -f slant 'D 3 V I 1' |lolcat
+echo
+
+echo "#------------------------------------------------#" |lolcat
+echo "#                   WH!73 D3V!1                  #" |lolcat
+echo "#------------------------------------------------#" |lolcat
+echo "#  Github  : github.com/WHI73-D3VI1/             #" |lolcat
+echo "# Facebook: https://www.facebook.com/WHI73.D3VI1 #" |lolcat
+echo "#  Telegram : https://t.me/WhiteDevil2026        #" |lolcat
+echo "#------------------------------------------------#" |lolcat
+echo "#              【﻿ IƬ'Ƨ MΣ §@ßßÌR 】             #" |lolcat
+echo "#------------------------------------------------#" |lolcat
+
+echo
+echo
+
+echo "Now Your Termux Is Ready To Run," |lolcat
+
+#WHI73 D3VI1
